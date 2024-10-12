dc5538f79ae1d0a84d2f67442df3c0c2bcf60f15
Ganesh <ganeshpodishetti@gmail.com>
2024-10-12 12:26:58 -0400

    completed the navbar

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import styled, { ThemeProvider } from "styled-components";
+import "./App.css";
+import Education from "./components/education";
+import Hero from "./components/hero";
+import NavBar from "./components/navbar";
+import Skills from "./components/skills";
+import { darkTheme } from "./utils/theme";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+        <NavBar />
+        <Body>
+          <Hero />
+          <Wrapper>
+            <Education />
+            <Skills />
+          </Wrapper>
+        </Body>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
